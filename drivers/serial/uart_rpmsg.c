963aeca6db34d75ba6cd31fe750ae3b145b93397
ligd <liguiding@pinecone.net>
2017-10-31 20:12:24 +0800

    Nuttx: add UART rpmsg driver
    Change-Id: Ie441e433229585a8cd3fb0405580db043d2b6ab5
    Signed-off-by: ligd <liguiding@pinecone.net>

---
+++ drivers/serial/uart_rpmsg.c
@@ -0,0 +1,460 @@
+/****************************************************************************
+ * drivers/serial/uart_rpmsg.c
+ * Serial driver for rpmsg UART
+ *
+ *   Copyright (C) 2017 Pinecone Inc. All rights reserved.
+ *   Author: Guiding Li<liguiding@pinecone.net>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <openamp/open_amp.h>
+
+#include <nuttx/kmalloc.h>
+#include <nuttx/serial/serial.h>
+#include <nuttx/serial/uart_rpmsg.h>
+
+/****************************************************************************
+ * Pre-processor definitions
+ ****************************************************************************/
+
+#define UART_RPMSG_DEV_CONSOLE          "/dev/console"
+#define UART_RPMSG_DEV_PREFIX           "/dev/ttyR"
+#define UART_RPMSG_CHANNEL_PREFIX       "rpmsg-tty"
+
+#define UART_RPMSG_TTY_WRITE            0
+#define UART_RPMSG_TTY_WAKEUP           1
+
+/****************************************************************************
+ * Private Types
+ ****************************************************************************/
+
+begin_packed_struct struct uart_rpmsg_header_s
+{
+  uint32_t command : 31;
+  uint32_t response : 1;
+  int32_t  result;
+  uint64_t cookie;
+} end_packed_struct;
+
+begin_packed_struct struct uart_rpmsg_write_s
+{
+  struct uart_rpmsg_header_s header;
+  uint32_t                   count;
+  uint32_t                   resolved;
+  char                       data[0];
+} end_packed_struct;
+
+begin_packed_struct struct uart_rpmsg_wakeup_s
+{
+  struct uart_rpmsg_header_s header;
+} end_packed_struct;
+
+struct uart_rpmsg_priv_s
+{
+  struct rpmsg_channel *channel;
+  bool                 master;
+  bool                 registered;
+  char                 channel_name[RPMSG_NAME_SIZE];
+  int                  dev_id;
+  int                  cpu_id;
+  void                 *recv_data;
+  bool                 last_upper;
+};
+
+/****************************************************************************
+ * Private Function Prototypes
+ ****************************************************************************/
+
+static int  uart_rpmsg_setup(FAR struct uart_dev_s *dev);
+static void uart_rpmsg_shutdown(FAR struct uart_dev_s *dev);
+static int  uart_rpmsg_attach(FAR struct uart_dev_s *dev);
+static void uart_rpmsg_detach(FAR struct uart_dev_s *dev);
+static int  uart_rpmsg_ioctl(FAR struct file *filep, int cmd, unsigned long arg);
+static void uart_rpmsg_rxint(FAR struct uart_dev_s *dev, bool enable);
+static bool uart_rpmsg_rxflowcontrol(FAR struct uart_dev_s *dev,
+                    unsigned int nbuffered, bool upper);
+static void uart_rpmsg_dmasend(FAR struct uart_dev_s *dev);
+static void uart_rpmsg_dmareceive(FAR struct uart_dev_s *dev);
+static void uart_rpmsg_dmarxfree(FAR struct uart_dev_s *dev);
+static void uart_rpmsg_dmatxavail(FAR struct uart_dev_s *dev);
+static void uart_rpmsg_txint(FAR struct uart_dev_s *dev, bool enable);
+static bool uart_rpmsg_txempty(FAR struct uart_dev_s *dev);
+static void uart_rpmsg_dmacontinue(FAR struct uart_dev_s *dev);
+static void uart_rpmsg_device_created(struct remote_device *rdev, void *priv_);
+static void uart_rpmsg_channel_created(struct rpmsg_channel *channel);
+static void uart_rpmsg_channel_destroyed(struct rpmsg_channel *channel);
+static void uart_rpmsg_channel_received(struct rpmsg_channel *channel,
+                    void *data, int len, void *priv, unsigned long src);
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+static const struct uart_ops_s g_uart_rpmsg_ops =
+{
+  .setup         = uart_rpmsg_setup,
+  .shutdown      = uart_rpmsg_shutdown,
+  .attach        = uart_rpmsg_attach,
+  .detach        = uart_rpmsg_detach,
+  .ioctl         = uart_rpmsg_ioctl,
+  .rxint         = uart_rpmsg_rxint,
+  .rxflowcontrol = uart_rpmsg_rxflowcontrol,
+  .dmasend       = uart_rpmsg_dmasend,
+  .dmareceive    = uart_rpmsg_dmareceive,
+  .dmarxfree     = uart_rpmsg_dmarxfree,
+  .dmatxavail    = uart_rpmsg_dmatxavail,
+  .txint         = uart_rpmsg_txint,
+  .txempty       = uart_rpmsg_txempty,
+};
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+static int uart_rpmsg_setup(FAR struct uart_dev_s *dev)
+{
+  return OK;
+}
+
+static void uart_rpmsg_shutdown(FAR struct uart_dev_s *dev)
+{
+}
+
+static int uart_rpmsg_attach(FAR struct uart_dev_s *dev)
+{
+  return OK;
+}
+
+static void uart_rpmsg_detach(FAR struct uart_dev_s *dev)
+{
+}
+
+static int uart_rpmsg_ioctl(FAR struct file *filep, int cmd, unsigned long arg)
+{
+  return -ENOTTY;
+}
+
+static void uart_rpmsg_rxint(FAR struct uart_dev_s *dev, bool enable)
+{
+}
+
+static bool uart_rpmsg_rxflowcontrol(FAR struct uart_dev_s *dev,
+                unsigned int nbuffered, bool upper)
+{
+  struct uart_rpmsg_priv_s *priv = dev->priv;
+  struct uart_rpmsg_wakeup_s msg;
+
+  if (!upper && upper != priv->last_upper)
+    {
+      memset(&msg, 0, sizeof(msg));
+
+      msg.header.command = UART_RPMSG_TTY_WAKEUP;
+      rpmsg_send(priv->channel, &msg, sizeof(msg));
+    }
+
+  priv->last_upper = upper;
+  return false;
+}
+
+static void uart_rpmsg_dmasend(FAR struct uart_dev_s *dev)
+{
+  struct uart_rpmsg_priv_s *priv = dev->priv;
+  struct uart_dmaxfer_s *xfer = &dev->dmatx;
+  struct uart_rpmsg_write_s *msg;
+  size_t len = xfer->length + xfer->nlength;
+  uint32_t space;
+
+  msg = rpmsg_get_tx_payload_buffer(priv->channel, &space, true);
+  if (!msg)
+    {
+      return;
+    }
+
+  memset(msg, 0, sizeof(*msg));
+
+  space = C2B(space - sizeof(*msg));
+
+  if (len > space)
+    {
+      len = space;
+    }
+
+  if (len > xfer->length)
+    {
+      cmem2bmem(msg->data, 0, xfer->buffer, xfer->length);
+      cmem2bmem(msg->data + B2C_OFF(xfer->length), B2C_REM(xfer->length),
+              xfer->nbuffer, len - xfer->length);
+    }
+  else
+    {
+      cmem2bmem(msg->data, 0, xfer->buffer, len);
+    }
+
+  msg->count          = len;
+  msg->header.command = UART_RPMSG_TTY_WRITE;
+  msg->header.result  = -ENXIO;
+  msg->header.cookie  = (uintptr_t)dev;
+
+  rpmsg_send_nocopy(priv->channel, msg, sizeof(*msg) + B2C(len));
+}
+
+static void uart_rpmsg_dmareceive(FAR struct uart_dev_s *dev)
+{
+  struct uart_rpmsg_priv_s *priv = dev->priv;
+  struct uart_dmaxfer_s *xfer = &dev->dmarx;
+  struct uart_rpmsg_write_s *msg = priv->recv_data;
+  uint32_t len = msg->count;
+  size_t space = xfer->length + xfer->nlength;
+
+  if (len > space)
+    {
+      len = space;
+    }
+
+  if (len > xfer->length)
+    {
+      bmem2cmem(xfer->buffer, msg->data, 0, xfer->length);
+      bmem2cmem(xfer->nbuffer, msg->data + B2C_OFF(xfer->length),
+              B2C_REM(xfer->length), len - xfer->length);
+    }
+  else
+    {
+      bmem2cmem(xfer->buffer, msg->data, 0, len);
+    }
+
+  xfer->nbytes = len;
+  uart_recvchars_done(dev);
+
+  msg->header.result = len;
+}
+
+static void uart_rpmsg_dmarxfree(FAR struct uart_dev_s *dev)
+{
+}
+
+static void uart_rpmsg_dmatxavail(FAR struct uart_dev_s *dev)
+{
+  struct uart_dmaxfer_s *xfer = &dev->dmatx;
+
+  if (xfer->length + xfer->nlength == 0)
+    {
+      uart_xmitchars_dma(dev);
+    }
+}
+
+static void uart_rpmsg_txint(FAR struct uart_dev_s *dev, bool enable)
+{
+}
+
+static bool uart_rpmsg_txempty(FAR struct uart_dev_s *dev)
+{
+  return true;
+}
+
+static void uart_rpmsg_dmacontinue(FAR struct uart_dev_s *dev)
+{
+  int ret;
+
+  ret = nxsem_trywait(&dev->xmit.sem);
+  if (ret == 0)
+    {
+      uart_rpmsg_dmatxavail(dev);
+      nxsem_post(&dev->xmit.sem);
+    }
+  else
+    {
+      uart_datasent(dev);
+    }
+}
+
+static void uart_rpmsg_device_created(struct remote_device *rdev, void *priv_)
+{
+  struct uart_dev_s *dev = priv_;
+  struct uart_rpmsg_priv_s *priv = dev->priv;
+
+  if (priv->master && priv->cpu_id == rdev->proc.cpu_id)
+    {
+      rpmsg_create_channel(rdev, priv->channel_name);
+    }
+}
+
+static void uart_rpmsg_channel_created(struct rpmsg_channel *channel)
+{
+  struct uart_dev_s *dev = rpmsg_get_callback_privdata(channel->name);
+  struct uart_rpmsg_priv_s *priv = dev->priv;
+  char dev_name[32];
+
+  priv->channel = channel;
+  rpmsg_set_privdata(channel, dev);
+
+  if (!priv->registered)
+    {
+      sprintf(dev_name, "%s%d", UART_RPMSG_DEV_PREFIX, priv->dev_id);
+      uart_register(dev_name, dev);
+
+      if (dev->isconsole)
+        {
+          uart_register(UART_RPMSG_DEV_CONSOLE, dev);
+        }
+
+      priv->registered = true;
+    }
+
+  uart_connected(dev, true);
+}
+
+static void uart_rpmsg_channel_destroyed(struct rpmsg_channel *channel)
+{
+  struct uart_dev_s *dev = rpmsg_get_privdata(channel);
+  struct uart_rpmsg_priv_s *priv = dev->priv;
+
+  uart_connected(dev, false);
+  priv->channel = NULL;
+}
+
+static void uart_rpmsg_channel_received(struct rpmsg_channel *channel,
+                    void *data, int len, void *priv_, unsigned long src)
+{
+  struct uart_dev_s *dev = rpmsg_get_privdata(channel);
+  struct uart_rpmsg_priv_s *priv = dev->priv;
+  struct uart_rpmsg_header_s *header = data;
+  struct uart_rpmsg_write_s *msg = data;
+
+  if (header->response)
+    {
+      dev->dmatx.nbytes = header->result;
+      if (header->result < 0)
+        {
+          dev->dmatx.nbytes = 0;
+        }
+
+      uart_xmitchars_done(dev);
+
+      if (msg->count == header->result)
+        {
+          uart_rpmsg_dmacontinue(dev);
+        }
+      return;
+    }
+
+  if (header->command == UART_RPMSG_TTY_WRITE)
+    {
+      priv->recv_data = data;
+      uart_recvchars_dma(dev);
+      priv->recv_data = NULL;
+
+      header->response = 1;
+      rpmsg_send(priv->channel, msg, sizeof(*msg));
+    }
+  else if (header->command == UART_RPMSG_TTY_WAKEUP)
+    {
+      uart_rpmsg_dmacontinue(dev);
+    }
+}
+
+/****************************************************************************
+ * Public Funtions
+ ****************************************************************************/
+
+int uart_rpmsg_init(int dev_id, int buf_size,
+        bool isconsole, int cpu_id, int ipc_id, bool master)
+{
+  struct uart_rpmsg_priv_s *priv;
+  struct uart_dev_s *dev;
+  int ret = -ENOMEM;
+
+  dev = kmm_zalloc(sizeof(struct uart_dev_s));
+  if (!dev)
+    {
+      return ret;
+    }
+
+  dev->ops       = &g_uart_rpmsg_ops;
+  dev->isconsole = isconsole;
+  dev->recv.size = buf_size;
+  dev->xmit.size = buf_size;
+
+  dev->recv.buffer = kmm_malloc(dev->recv.size);
+  if (!dev->recv.buffer)
+    {
+      goto fail;
+    }
+
+  dev->xmit.buffer = kmm_malloc(dev->xmit.size);
+  if (!dev->xmit.buffer)
+    {
+      goto fail;
+    }
+
+  priv = kmm_zalloc(sizeof(struct uart_rpmsg_priv_s));
+  if (!priv)
+    {
+      goto fail;
+    }
+
+  priv->master = master;
+  priv->dev_id = dev_id;
+  priv->cpu_id = cpu_id;
+
+  dev->priv    = priv;
+
+  sprintf(priv->channel_name, "%s%d", UART_RPMSG_CHANNEL_PREFIX, ipc_id);
+  ret = rpmsg_register_callback(priv->channel_name, dev,
+              uart_rpmsg_device_created,
+              NULL,
+              uart_rpmsg_channel_created,
+              uart_rpmsg_channel_destroyed,
+              uart_rpmsg_channel_received);
+  if (ret < 0)
+    {
+      goto fail;
+    }
+
+  return OK;
+
+fail:
+  kmm_free(dev->recv.buffer);
+  kmm_free(dev->xmit.buffer);
+  kmm_free(dev->priv);
+  kmm_free(dev);
+
+  return ret;
+}
+
