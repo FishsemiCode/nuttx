b043f49221f2cb70bd010696686fe3bf2b547efa
ligd <liguiding@pinecone.net>
2017-12-08 20:43:51 +0800

    Nuttx: add song remote proc tunnel support
    Change-Id: Id85c22e9197f941f6330e8eaf6a30cbadfbf3257
    Signed-off-by: ligd <liguiding@pinecone.net>

---
+++ drivers/rptun/song_rptun.c
@@ -0,0 +1,223 @@
+/****************************************************************************
+ * drivers/rptun/song_rptun.c
+ *
+ *   Copyright (C) 2017 Pinecone Inc. All rights reserved.
+ *   Author: Guiding Li<liguiding@pinecone.net>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <nuttx/arch.h>
+#include <nuttx/kmalloc.h>
+#include <nuttx/rptun/song_rptun.h>
+
+#include <errno.h>
+#include <string.h>
+
+/****************************************************************************
+ * Private Types
+ ****************************************************************************/
+
+struct song_rptun_dev_s
+{
+  struct rptun_dev_s               rptun;
+  const struct song_rptun_config_s *config;
+  uint32_t                         count_rx;
+  rptun_callback_t                 callback;
+  void                             *arg;
+};
+
+/************************************************************************************
+ * Private Function Prototypes
+ ************************************************************************************/
+
+static int song_rptun_get_resource(struct rptun_dev_s *dev,
+                                    struct rsc_table_info *rsc, uint32_t *role);
+static int song_rptun_get_sharemem(struct rptun_dev_s *dev,
+                                    struct proc_shm *shm);
+static int song_rptun_notify(struct rptun_dev_s *dev, uint32_t vqid);
+static int song_rptun_registercallback(struct rptun_dev_s *dev,
+                                    rptun_callback_t callback, void *arg);
+static int song_rptun_ioctl(struct rptun_dev_s *dev, int cmd, unsigned long arg);
+static int song_rptun_start_isr(void *arg, uintptr_t msg);
+static int song_rptun_vring_isr(void *arg, uintptr_t msg);
+
+/************************************************************************************
+ * Private Data
+ ************************************************************************************/
+
+static const struct rptun_ops_s g_song_rptun_ops =
+{
+  .get_resource      = song_rptun_get_resource,
+  .get_sharemem      = song_rptun_get_sharemem,
+  .notify            = song_rptun_notify,
+  .register_callback = song_rptun_registercallback,
+  .ioctl             = song_rptun_ioctl,
+};
+
+/************************************************************************************
+ * Private Functions
+ ************************************************************************************/
+
+static int song_rptun_get_resource(struct rptun_dev_s *dev,
+                                    struct rsc_table_info *rsc, uint32_t *role)
+{
+  struct song_rptun_dev_s *priv = (struct song_rptun_dev_s *)dev;
+  const struct song_rptun_config_s *config = priv->config;
+
+  memcpy(rsc, &config->rsc, sizeof(config->rsc));
+  *role = config->role;
+
+  return 0;
+}
+
+static int song_rptun_get_sharemem(struct rptun_dev_s *dev,
+                                    struct proc_shm *shm)
+{
+  struct song_rptun_dev_s *priv = (struct song_rptun_dev_s *)dev;
+  const struct song_rptun_config_s *config = priv->config;
+
+  memcpy(shm, &config->shm, sizeof(config->shm));
+
+  return 0;
+}
+
+static int song_rptun_notify(struct rptun_dev_s *dev, uint32_t vqid)
+{
+  struct song_rptun_dev_s *priv = (struct song_rptun_dev_s *)dev;
+  const struct song_rptun_config_s *config = priv->config;
+  int ret;
+
+  if (vqid == RPTUN_NOTIFY_START && config->ch_start_tx >= 0)
+    {
+      ret = MBOX_SEND(config->mbox_tx, config->ch_start_tx, 0);
+    }
+  else
+    {
+      ret = MBOX_SEND(config->mbox_tx, config->ch_vring_tx, 0);
+    }
+
+  return ret;
+}
+
+static int song_rptun_registercallback(struct rptun_dev_s *dev,
+                                    rptun_callback_t callback, void *arg)
+{
+  struct song_rptun_dev_s *priv = (struct song_rptun_dev_s *)dev;
+  const struct song_rptun_config_s *config = priv->config;
+  int ret = 0;
+
+  priv->callback = callback;
+  priv->arg      = arg;
+
+  if (config->ch_start_rx >= 0)
+    {
+      ret |= MBOX_REGISTER_CALLBACK(config->mbox_rx, config->ch_start_rx,
+                        callback ? song_rptun_start_isr : NULL, priv);
+    }
+
+  ret |= MBOX_REGISTER_CALLBACK(config->mbox_rx, config->ch_vring_rx,
+                    callback ? song_rptun_vring_isr : NULL, priv);
+
+  return ret;
+}
+
+static int song_rptun_ioctl(struct rptun_dev_s *dev, int cmd, unsigned long arg)
+{
+  if (cmd == RPTUN_USER_IOCBASE)
+    {
+      *((void **)(uintptr_t)arg) = dev;
+      return 0;
+    }
+
+  return -ENOTTY;
+}
+
+static int song_rptun_start_isr(void *arg, uintptr_t msg)
+{
+  struct song_rptun_dev_s *priv = arg;
+
+  if (priv->count_rx != 0)
+    {
+      priv->count_rx = 0;
+      if (priv->callback)
+        {
+          priv->callback(priv->arg, RPTUN_NOTIFY_START);
+        }
+    }
+
+  return 0;
+}
+
+static int song_rptun_vring_isr(void *arg, uintptr_t msg)
+{
+  struct song_rptun_dev_s *priv = arg;
+
+  priv->count_rx++;
+  if (priv->callback)
+    {
+      priv->callback(priv->arg, RPTUN_NOTIFY_ALL);
+    }
+
+  return 0;
+}
+
+/************************************************************************************
+ * Public Functions
+ ************************************************************************************/
+
+struct rptun_dev_s *song_rptun_initialize(const struct song_rptun_config_s *config, int minor)
+{
+  struct song_rptun_dev_s *priv;
+  int ret;
+
+  priv = kmm_zalloc(sizeof(struct song_rptun_dev_s));
+  if (priv == NULL)
+    {
+      return NULL;
+    }
+
+  priv->rptun.ops = &g_song_rptun_ops;
+  priv->config    = config;
+
+  ret = rptun_register((struct rptun_dev_s *)priv, minor);
+  if (ret < 0)
+    {
+      kmm_free(priv);
+      return NULL;
+    }
+
+  return (struct rptun_dev_s *)priv;
+}
---
+++ include/nuttx/rptun/song_rptun.h
@@ -0,0 +1,90 @@
+/********************************************************************************************
+ * include/nuttx/rptun/song_rptun.h
+ *
+ *   Copyright (C) 2017 Pinecone Inc. All rights reserved.
+ *   Author: Guiding Li<liguiding@pinecone.net>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ********************************************************************************************/
+
+#ifndef __INCLUDE_NUTTX_RPTUN_SONG_RPTUN_H
+#define __INCLUDE_NUTTX_RPTUN_SONG_RPTUN_H
+
+#ifdef CONFIG_SONG_RPTUN
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <nuttx/rptun/rptun.h>
+#include <nuttx/mbox/mbox.h>
+
+/****************************************************************************
+ * Public Types
+ ****************************************************************************/
+
+struct song_rptun_config_s
+{
+  uint32_t              role;
+  int32_t               ch_start_rx;
+  int32_t               ch_vring_rx;
+  struct mbox_dev_s     *mbox_rx;
+  int32_t               ch_start_tx;
+  int32_t               ch_vring_tx;
+  struct mbox_dev_s     *mbox_tx;
+
+  /* for talk between peer processor, like tl420 and cp */
+  struct proc_shm       shm;
+
+  /* the resource table info */
+  struct rsc_table_info rsc;
+};
+
+/****************************************************************************
+ * Public Function Prototypes
+ ****************************************************************************/
+
+#ifdef __cplusplus
+#define EXTERN extern "C"
+extern "C"
+{
+#else
+#define EXTERN extern
+#endif
+
+struct rptun_dev_s *song_rptun_initialize(const struct song_rptun_config_s *config, int minor);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CONFIG_SONG_RPTUN */
+#endif /* __INCLUDE_NUTTX_RPTUN_SONG_RPTUN_H */
