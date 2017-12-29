eb166b9aff4be4328cc30ed5c8f05696c10e1e76
ligd <liguiding@pinecone.net>
2017-12-29 20:50:32 +0800

    Nuttx: modify HP & LP workqueue don't init to zero
    Change-Id: I1d8b8315b6c5fb9692b7ed707382c2dfda131c1a
    Signed-off-by: ligd <liguiding@pinecone.net>

--- sched/wqueue/kwork_lpthread.c
+++ sched/wqueue/kwork_lpthread.c
@@ -184,10 +184,7 @@ int work_lpstart(void)
 
   /* Initialize work queue data structures */
 
-  memset(&g_lpwork, 0, sizeof(struct kwork_wqueue_s));
-
   g_lpwork.delay = CONFIG_SCHED_LPWORKPERIOD / USEC_PER_TICK;
-  dq_init(&g_lpwork.q);
 
   /* Don't permit any of the threads to run until we have fully initialized
    * g_lpwork.
