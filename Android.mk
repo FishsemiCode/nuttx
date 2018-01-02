203ab1c140fb85d2f1caf4e3279f0d1e5c0963f9
anchao <anchao@pinecone.net>
2018-01-02 10:33:16 +0800

    nuttx/build: add android build support
    Change-Id: I12b9a5ba6f72b5f1f3813e37938d2d0734d7f6ec
    Signed-off-by: anchao <anchao@pinecone.net>

---
+++ Android.mk
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2017 Pinecone
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+NUTTX_ROOT      := $(abspath $(call my-dir))
+NUTTX_OUTDIR    := $(abspath $(TARGET_OUT_INTERMEDIATES)/NUTTX_OBJ)
+
+NUTTX_ARMTOOL   := CROSSDEV=$(NUTTX_ROOT)/prebuilts/gcc/linux/arm/bin/arm-none-eabi-
+NUTTX_ARMTOOL   += ARCROSSDEV=$(NUTTX_ROOT)/prebuilts/gcc/linux/arm/bin/arm-none-eabi-
+
+#banks/rpm
+$(NUTTX_OUTDIR)/banks/rpm/nuttx/nuttx: FORCE
+	$(hide) $(NUTTX_ARMTOOL) $(NUTTX_ROOT)/nuttx/tools/configure.sh -o $(dir $@) banks/rpm
+	$(hide) $(NUTTX_ARMTOOL) $(MAKE) -C $(NUTTX_ROOT)/nuttx O=$(dir $@)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE               := banks-rpm.fw
+LOCAL_PREBUILT_MODULE_FILE := $(NUTTX_OUTDIR)/banks/rpm/nuttx/nuttx
+LOCAL_MODULE_CLASS         := FIRMWARE
+LOCAL_MODULE_PATH          := $(TARGET_OUT_VENDOR)/firmware
+include $(BUILD_PREBUILT)
+
+#banks/sensor
+$(NUTTX_OUTDIR)/banks/sensor/nuttx/nuttx: FORCE
+	$(hide) $(NUTTX_ARMTOOL) $(NUTTX_ROOT)/nuttx/tools/configure.sh -o $(dir $@) banks/sensor
+	$(hide) $(NUTTX_ARMTOOL) $(MAKE) -C $(NUTTX_ROOT)/nuttx O=$(dir $@)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE               := banks-sensor.fw
+LOCAL_PREBUILT_MODULE_FILE := $(NUTTX_OUTDIR)/banks/sensor/nuttx/nuttx
+LOCAL_MODULE_CLASS         := FIRMWARE
+LOCAL_MODULE_PATH          := $(TARGET_OUT_VENDOR)/firmware
+include $(BUILD_PREBUILT)
