3de2091f2fe7f826f154dbf3028af55f46fa95e6
Xiang Xiao <xiaoxiang@xiaomi.com>
2018-03-27 21:49:02 +0800

    song/u1: change AT input clock to 8.192MHz
    due to the hardware change
    
    Change-Id: Iec9fb2a8962a94bb0549b460a0896bd02f3a5ef1
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- arch/arm/src/song/u1_cp.c
+++ arch/arm/src/song/u1_cp.c
@@ -122,7 +122,7 @@ void arm_timer_initialize(void)
     .base       = 0xb0040000,
     .irq        = 18,
     .c1_max     = 2048,
-    .c1_freq    = 32768000,
+    .c1_freq    = 8192000,
     .ctl_off    = 0x170,
     .calib_off  = 0x194,
     .c1_off     = 0x174,
