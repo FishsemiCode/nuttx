b8845a8efb233b6203a05db2de5da777dc82d8ba
wangyanjiong <wangyanjiong@xiaomi.com>
2018-03-30 20:48:38 +0800

    arch/song: add initial u2 support
    Change-Id: Id5166684e35ce4e4498626a463f3182cf1e30514
    Signed-off-by: wangyanjiong <wangyanjiong@xiaomi.com>

---
+++ arch/arm/src/song/u2_ap.c
@@ -0,0 +1,92 @@
+/****************************************************************************
+ * arch/arm/src/song/u2_ap.c
+ *
+ *   Copyright (C) 2017 Pinecone Inc. All rights reserved.
+ *   Author: Wang Yanjiong <wangyanjiong@pinecone.net>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+#include <nuttx/timers/arch_alarm.h>
+#include <nuttx/timers/song_oneshot.h>
+
+#include "up_arch.h"
+#include "up_internal.h"
+
+#ifdef CONFIG_ARCH_CHIP_U2_AP
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define TOP_PWR_BASE                (0xa00e0000)
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+void arm_timer_initialize(void)
+{
+  static const struct song_oneshot_config_s config =
+  {
+    .base       = TOP_PWR_BASE,
+    .irq        = 23,
+    .c1_max     = 2048,
+    .c1_freq    = 32768000,
+    .ctl_off    = 0x290,
+    .calib_off  = 0x2b4,
+    .c1_off     = 0x294,
+    .c2_off     = 0x298,
+    .spec_off   = 0x2cc,
+    .intren_off = 0x214,
+    .intrst_off = 0x21c,
+    .intr_bit   = 2,
+  };
+
+  up_alarm_set_lowerhalf(song_oneshot_initialize(&config, -1));
+}
+
+#endif /* CONFIG_ARCH_CHIP_U2_AP */
