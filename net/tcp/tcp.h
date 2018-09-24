23d74144c26042757af33d197d06a1eb2413b9d9
Xiang Xiao <xiaoxiang@xiaomi.com>
2018-09-24 23:42:56 +0800

    fix compiler error and warn when CONFIG_NET_SENDFILE=y
    Change-Id: Iaa023148d7a041dee4f019323d55d142b286be31
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- net/tcp/tcp.h
+++ net/tcp/tcp.h
@@ -322,6 +322,7 @@ EXTERN struct net_driver_s *g_netdevices;
  * Public Function Prototypes
  ****************************************************************************/
 
+struct file;      /* Forward reference */
 struct sockaddr;  /* Forward reference */
 struct socket;    /* Forward reference */
 struct pollfd;    /* Forward reference */
