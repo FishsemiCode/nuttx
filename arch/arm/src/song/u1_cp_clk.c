0e60018ea3dbcb46d60b91ab84e8bda5e368f2af
xuanlin <xuanlin@pinecone.net>
2018-10-10 19:21:38 +0800

    cp: enable cp_pclk by default.
    Change-Id: I0ea784295149ea0df836ab7c0df9c5b24588d94a
    Signed-off-by: xuanlin <xuanlin@pinecone.net>

--- arch/arm/src/song/u1_cp_clk.c
+++ arch/arm/src/song/u1_cp_clk.c
@@ -188,6 +188,7 @@ static const struct song_div_clk div[] =
     .div_offset = 0x060,
     .div_shift = 4,
     .div_width = 3,
+    .clk_flags = CLK_IGNORE_UNUSED,
   },
   {
     .name = "sim_clk",
