87903919819b1697679d2df18e85c24efc57d5bd
Xiang Xiao <xiaoxiang@xiaomi.com>
2018-12-12 12:27:03 +0800

    Make romfs support mtd driver interface too
    Change-Id: Ic7146e57a30160040cd24ab2e54813e3ece03c2b
    Signed-off-by: Xiang Xiao <xiaoxiang@xiaomi.com>

--- fs/mount/fs_mount.c
+++ fs/mount/fs_mount.c
@@ -142,6 +142,9 @@ static const struct fsmap_t g_bdfsmap[] =
 #ifdef MDFS_SUPPORT
 /* File systems that require MTD drivers */
 
+#ifdef CONFIG_FS_ROMFS
+extern const struct mountpt_operations romfs_operations;
+#endif
 #ifdef CONFIG_FS_SPIFFS
 extern const struct mountpt_operations spiffs_operations;
 #endif
@@ -151,6 +154,9 @@ extern const struct mountpt_operations littlefs_operations;
 
 static const struct fsmap_t g_mdfsmap[] =
 {
+#ifdef CONFIG_FS_ROMFS
+    { "romfs", &romfs_operations },
+#endif
 #ifdef CONFIG_FS_SPIFFS
     { "spiffs", &spiffs_operations },
 #endif
