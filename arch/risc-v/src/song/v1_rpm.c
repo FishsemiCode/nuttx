51438fcc4168819e38a5d08542a4dbe55c16481d
liuhaiyang <liuhaiyang@xiaomi.com>
2018-12-14 11:31:21 +0800

    v1:rpm: add rpm-ri5cy support for v1
    N/A
    
    Change-Id: I4927d9b90cbbe9193a25137f2ad91d45e33170d9
    Signed-off-by: liuhaiyang <liuhaiyang@xiaomi.com>

---
+++ arch/risc-v/src/song/v1_rpm.c
@@ -0,0 +1,91 @@
+/****************************************************************************
+ * arch/risc-v/src/song/v1_rpm.c
+ *
+ *   Copyright (C) 2018 Pinecone Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name NuttX nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/****************************************************************************
+ * Included Files
+ ****************************************************************************/
+
+#include <nuttx/config.h>
+
+#include <nuttx/timers/arch_alarm.h>
+#include <nuttx/timers/song_oneshot.h>
+
+#include "up_internal.h"
+
+#ifdef CONFIG_ARCH_CHIP_V1_RPM
+
+/****************************************************************************
+ * Pre-processor Definitions
+ ****************************************************************************/
+
+#define TOP_PWR_BASE                (0xf9210000)
+
+/****************************************************************************
+ * Private Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Data
+ ****************************************************************************/
+
+/****************************************************************************
+ * Public Functions
+ ****************************************************************************/
+
+void riscv_timer_initialize(void)
+{
+#ifdef CONFIG_ONESHOT_SONG
+  static const struct song_oneshot_config_s config =
+  {
+    .minor      = -1,
+    .base       = TOP_PWR_BASE,
+    .irq        = 5,
+    .c1_max     = 2600,
+    .c1_freq    = 26000000,
+    .ctl_off    = 0x160,
+    .calib_off  = 0x174,
+    .c1_off     = 0x164,
+    .c2_off     = 0x168,
+    .spec_off   = 0x208,
+    .intren_off = 0x150,
+    .intrst_off = 0x154,
+    .intr_bit   = 26,
+  };
+
+  up_alarm_set_lowerhalf(song_oneshot_initialize(&config));
+#endif
+
+}
+
+#endif /* CONFIG_ARCH_CHIP_V1_RPM */
