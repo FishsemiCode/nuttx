28f76910726085eeaca368224deae5a69eb975e1
ligd <liguiding@pinecone.net>
2019-01-17 20:45:59 +0800

    clk: fix compile failed when not define CONFIG_CLK
    Change-Id: I9d994e3195a9043bfab163433780b27fd10cebb7
    Signed-off-by: ligd <liguiding@pinecone.net>

--- include/nuttx/clk/clk.h
+++ include/nuttx/clk/clk.h
@@ -153,7 +153,7 @@ static inline void clk_disable_unused(void)
   }
 static inline const char *clk_get_name(const struct clk *clk)
   {
-    return (struct char *)(-ENOENT);
+    return (const char *)(-ENOENT);
   }
 #endif
 
