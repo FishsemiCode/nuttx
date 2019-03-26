b50b614e16fe03886be9e58fdff0e2ebd897799e
ZhongAn <zhongan@pinecone.net>
2019-03-26 16:45:45 +0800

    audio: remove unused macros.
    Change-Id: I6dd733fddfc5016f8547c4189b132034f4295037
    Signed-off-by: ZhongAn <zhongan@pinecone.net>

--- include/nuttx/audio/audio.h
+++ include/nuttx/audio/audio.h
@@ -123,8 +123,6 @@
 #define AUDIOIOC_UNREGISTERMQ       _AUDIOIOC(15)
 #define AUDIOIOC_HWRESET            _AUDIOIOC(16)
 #define AUDIOIOC_SETBUFFERINFO      _AUDIOIOC(17)
-#define AUDIOIOC_GETREGVALUE        _AUDIOIOC(18)
-#define AUDIOIOC_SETREGVALUE        _AUDIOIOC(19)
 
 /* Audio Device Types *******************************************************/
 /* The NuttX audio interface support different types of audio devices for
