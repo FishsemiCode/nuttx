41788b83ff9bf83ca1f990724930a62ccf264016
doujianglong <doujianglong@fishsemi.com>
2019-07-26 15:50:35 +0800

    v1: isp: add i2c config for v1 isp
    Change-Id: Ia9d3046a5c9f8a395f57099d8eb35662e3b637a9

--- arch/risc-v/src/song/v1_isp.c
+++ arch/risc-v/src/song/v1_isp.c
@@ -43,6 +43,30 @@
 
 #include <nuttx/timers/arch_alarm.h>
 #include <nuttx/timers/song_oneshot.h>
+#include <nuttx/clk/clk-provider.h>
+#include <nuttx/dma/song_dmas.h>
+#include <nuttx/fs/fs.h>
+#include <nuttx/fs/hostfs_rpmsg.h>
+#include <nuttx/fs/partition.h>
+#include <nuttx/i2c/i2c_dw.h>
+#include <nuttx/ioexpander/song_ioe.h>
+#include <nuttx/mbox/song_mbox.h>
+#include <nuttx/mtd/mtd.h>
+#include <nuttx/power/regulator.h>
+#include <nuttx/pwm/song_pwm.h>
+#include <nuttx/rptun/song_rptun.h>
+#include <nuttx/serial/uart_16550.h>
+#include <nuttx/serial/uart_rpmsg.h>
+#include <nuttx/spi/spi_dw.h>
+#include <nuttx/syslog/syslog_rpmsg.h>
+#include <nuttx/timers/arch_alarm.h>
+#include <nuttx/timers/dw_wdt.h>
+#include <nuttx/timers/song_oneshot.h>
+
+#include <stdio.h>
+
+#include "chip.h"
+#include "up_arch.h"
 
 #include "up_internal.h"
 
@@ -55,10 +79,41 @@
 /****************************************************************************
  * Private Data
  ****************************************************************************/
+#ifdef CONFIG_I2C_DW
+FAR struct i2c_master_s *g_i2c[3] =
+{
+  [2] = DEV_END,
+};
+#endif
 
 /****************************************************************************
  * Public Data
  ****************************************************************************/
+#ifdef CONFIG_I2C_DW
+static void up_i2c_init(void)
+{
+    static const struct dw_i2c_config_s config[] =
+    {
+        {
+            .bus = 0,
+            .base = 0xfa3e2000,
+            .rate = 26000000,
+            .mclk = "isp_i2c0_mclk",
+            .irq = 17,
+        },
+        {
+            .bus = 1,
+            .base = 0xfa3e3000,
+            .rate = 26000000,
+            .mclk = "isp_i2c1_mclk",
+            .irq = 18,
+        }
+    };
+  int config_num = sizeof(config) / sizeof(config[0]);
+
+  dw_i2c_allinitialize(config, config_num, g_i2c);
+}
+#endif
 
 /****************************************************************************
  * Public Functions
@@ -87,6 +142,13 @@ void riscv_timer_initialize(void)
   up_alarm_set_lowerhalf(song_oneshot_initialize(&config));
 #endif
 
+}
+
+void up_lateinitialize(void)
+{
+#ifdef CONFIG_I2C_DW
+    up_i2c_init();
+#endif
 }
 
 #endif /* CONFIG_ARCH_CHIP_V1_ISP */
